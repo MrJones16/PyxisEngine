9760f8ce8411fc9f2406de9090bc8b0e67ae736e
Peyton <projectile212@gmail.com>
2023-12-19 15:44:25 -0800

    Modified ImGui colors more, and updated peek image

--- Pyxis/src/Pyxis/ImGui/ImGuiLayer.cpp
+++ Pyxis/src/Pyxis/ImGui/ImGuiLayer.cpp
@@ -58,19 +58,19 @@ namespace Pyxis
 		colors[ImGuiCol_TitleBg] = ImVec4(0.55f, 0.64f, 0.57f, 1.00f);
 		colors[ImGuiCol_TitleBgActive] = ImVec4(0.55f, 0.64f, 0.57f, 1.00f);
 		colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.55f, 0.64f, 0.57f, 1.00f);
-		colors[ImGuiCol_MenuBarBg] = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+		colors[ImGuiCol_MenuBarBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.59f);
 		colors[ImGuiCol_ScrollbarBg] = ImVec4(0.98f, 0.98f, 0.98f, 0.53f);
 		colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.69f, 0.69f, 0.69f, 0.80f);
 		colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.49f, 0.49f, 0.49f, 0.80f);
 		colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.49f, 0.49f, 0.49f, 1.00f);
-		colors[ImGuiCol_CheckMark] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+		colors[ImGuiCol_CheckMark] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
 		colors[ImGuiCol_SliderGrab] = ImVec4(0.55f, 0.64f, 0.57f, 1.00f);
 		colors[ImGuiCol_SliderGrabActive] = ImVec4(0.55f, 0.64f, 0.57f, 1.00f);
-		colors[ImGuiCol_Button] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
-		colors[ImGuiCol_ButtonHovered] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-		colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
-		colors[ImGuiCol_Header] = ImVec4(0.26f, 0.59f, 0.98f, 0.31f);
-		colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+		colors[ImGuiCol_Button] = ImVec4(1.00f, 0.98f, 0.90f, 1.00f);
+		colors[ImGuiCol_ButtonHovered] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+		colors[ImGuiCol_ButtonActive] = ImVec4(0.55f, 0.64f, 0.57f, 1.00f);
+		colors[ImGuiCol_Header] = ImVec4(0.55f, 0.64f, 0.57f, 0.59f);
+		colors[ImGuiCol_HeaderHovered] = ImVec4(0.55f, 0.64f, 0.57f, 0.78f);
 		colors[ImGuiCol_HeaderActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
 		colors[ImGuiCol_Separator] = ImVec4(0.39f, 0.39f, 0.39f, 0.62f);
 		colors[ImGuiCol_SeparatorHovered] = ImVec4(0.14f, 0.44f, 0.80f, 0.78f);
@@ -78,11 +78,11 @@ namespace Pyxis
 		colors[ImGuiCol_ResizeGrip] = ImVec4(0.35f, 0.35f, 0.35f, 0.17f);
 		colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
 		colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
-		colors[ImGuiCol_Tab] = ImVec4(0.76f, 0.80f, 0.84f, 0.93f);
+		colors[ImGuiCol_Tab] = ImVec4(0.55f, 0.64f, 0.57f, 1.00f);
 		colors[ImGuiCol_TabHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
-		colors[ImGuiCol_TabActive] = ImVec4(0.60f, 0.73f, 0.88f, 1.00f);
-		colors[ImGuiCol_TabUnfocused] = ImVec4(0.92f, 0.93f, 0.94f, 0.99f);
-		colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.74f, 0.82f, 0.91f, 1.00f);
+		colors[ImGuiCol_TabActive] = ImVec4(0.67f, 0.76f, 0.69f, 1.00f);
+		colors[ImGuiCol_TabUnfocused] = ImVec4(0.88f, 0.91f, 0.89f, 0.78f);
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.88f, 0.91f, 0.76f, 1.00f);
 		colors[ImGuiCol_DockingPreview] = ImVec4(1.00f, 0.96f, 0.88f, 0.39f);
 		colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
 		colors[ImGuiCol_PlotLines] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
@@ -100,6 +100,8 @@ namespace Pyxis
 		colors[ImGuiCol_NavWindowingHighlight] = ImVec4(0.70f, 0.70f, 0.70f, 0.70f);
 		colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.20f, 0.20f, 0.20f, 0.20f);
 		colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+
+
 
 
 		
