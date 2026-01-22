9f4875915190b60abee0ec16c364861fae785b4c
MrJones16 <peytonjamesjones@gmail.com>
2026-01-21 22:09:30 -0800

    Beginning work on Deferred rendering. Made the shader.

---
+++ Pixel-Game-Core/assets/shaders/Deferred.glsl
@@ -0,0 +1,62 @@
+#type vertex
+#version 460
+			
+layout (location = 0) in vec3 a_Position;
+layout (location = 1) in vec3 a_Normal;
+layout (location = 2) in vec4 a_Albedo;
+layout (location = 3) in vec2 a_TexCoord;
+layout (location = 4) in float a_TexIndex;
+layout (location = 5) in float a_TilingFactor;
+layout (location = 6) in uint a_NodeID;
+
+uniform mat4 u_ViewProjection;
+
+out vec4 v_Position;
+out vec4 v_Normal;
+out vec4 v_Albedo;
+out vec2 v_TexCoord;
+out float v_TexIndex;
+out float v_TilingFactor;
+out flat uint v_NodeID;
+
+void main()
+{
+	gl_Position = u_ViewProjection * vec4(a_Position, 1.0f);
+	v_Position = u_ViewProjection * vec4(a_Position, 1.0f);
+	v_Normal = vec4(a_Normal, 1.0f);
+	v_Albedo = a_Albedo;
+	v_TexCoord = a_TexCoord;
+	v_TexIndex = a_TexIndex;
+	v_TilingFactor = a_TilingFactor;
+	v_NodeID = a_NodeID;
+}
+
+#type fragment
+#version 460
+			
+layout (location = 0) out vec4 o_Position;
+layout (location = 1) out vec4 o_Normal;
+layout (location = 2) out vec4 o_Albedo;
+layout (location = 3) out uint o_ID;
+
+in vec4 v_Position;
+in vec4 v_Normal;
+in vec4 v_Albedo;
+in vec2 v_TexCoord;
+in float v_TexIndex;
+in float v_TilingFactor;
+in flat uint v_NodeID;
+
+uniform sampler2D u_Textures[32];
+
+void main()
+{
+	vec4 result = texture(u_Textures[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
+	if(result.a == 0)
+		discard;
+
+    o_Position = v_Position;
+    o_Normal = v_Normal;
+	o_Albedo = result;
+	o_ID = v_NodeID;
+}
--- Pyxis/src/Pyxis/Renderer/FrameBuffer.h
+++ Pyxis/src/Pyxis/Renderer/FrameBuffer.h
@@ -1,93 +1,83 @@
 #pragma once
 #include "Texture.h"
 
-namespace Pyxis
-{
+namespace Pyxis {
 
-	enum class FrameBufferTextureFormat
-	{
-		None = 0,
+enum class FrameBufferTextureFormat {
+    None = 0,
 
-		//color
-		RGBA8,
-		R32I,
-		R32UI,
+    // color
+    RGBA8,
+    R32I,
+    R32UI,
 
-		//depth / stencil
-		DEPTH24STENCIL8,
+    // depth / stencil
+    DEPTH24STENCIL8,
 
-		//defaults
-		Depth = DEPTH24STENCIL8,
-	};
+    // defaults
+    Depth = DEPTH24STENCIL8,
+};
 
-	enum class FrameBufferTextureType
-	{
-		None = 0,
-		Color,
-		Depth,
-	};
+enum class FrameBufferTextureType {
+    None = 0,
+    Color,
+    Depth,
+};
 
-	struct FrameBufferTextureSpecification
-	{
-		FrameBufferTextureSpecification() = default;
-		FrameBufferTextureSpecification(FrameBufferTextureFormat format, FrameBufferTextureType type)
-			: TextureFormat(format), TextureType(type)
-		{
+struct FrameBufferTextureSpecification {
+    FrameBufferTextureSpecification() = default;
+    FrameBufferTextureSpecification(FrameBufferTextureFormat format,
+                                    FrameBufferTextureType type)
+        : TextureFormat(format), TextureType(type) {}
 
-		}
+    FrameBufferTextureFormat TextureFormat;
+    FrameBufferTextureType TextureType;
 
-		FrameBufferTextureFormat TextureFormat;
-		FrameBufferTextureType TextureType;
+    // could have things like wrap, clamp/ ect
+};
 
-		//could have things like wrap, clamp/ ect
-	}; 
+struct FrameBufferAttachmentSpecification {
+    FrameBufferAttachmentSpecification() = default;
 
-	
+    FrameBufferAttachmentSpecification(
+        std::initializer_list<FrameBufferTextureSpecification> attachments)
+        : Attachments(attachments) {};
 
-	struct FrameBufferAttachmentSpecification
-	{
-		FrameBufferAttachmentSpecification() = default;
+    std::vector<FrameBufferTextureSpecification> Attachments;
+};
 
-		FrameBufferAttachmentSpecification(std::initializer_list<FrameBufferTextureSpecification> attachments)
-			: Attachments(attachments) {};
+struct FrameBufferSpecification {
+    uint32_t Width, Height;
+    FrameBufferAttachmentSpecification Attachments;
+    // idk
+    uint32_t samples = 1;
+    bool SwapChainTarget = false;
+};
 
+class FrameBuffer {
+  public:
+    virtual ~FrameBuffer() {}
 
-		std::vector<FrameBufferTextureSpecification> Attachments;
-	};
+    virtual void Invalidate() = 0;
 
-	struct FrameBufferSpecification
-	{
-		uint32_t Width, Height;
-		FrameBufferAttachmentSpecification Attachments;
-		//idk
-		uint32_t samples = 1;
-		bool SwapChainTarget = false;
-	};
+    virtual void Bind() const = 0;
+    virtual void Unbind() const = 0;
 
+    virtual void Resize(uint32_t width, uint32_t height) = 0;
 
-	class FrameBuffer
-	{
-	public:
-		virtual ~FrameBuffer() {}
+    virtual void ReadPixel(uint32_t attachmentIndex, int x, int y,
+                           void *data) = 0;
+    virtual void ReadPixels(uint32_t attachmentIndex, int x, int y, int width,
+                            int height, void *data) = 0;
 
-		virtual void Invalidate() = 0;
+    virtual void ClearColorAttachment(int index, const void *value) = 0;
 
-		virtual void Bind() const = 0;
-		virtual void Unbind() const = 0;
+    virtual uint32_t GetColorAttachmentRendererID(int index = 0) const = 0;
+    virtual void BindColorAttachmentTexture(int index = 0) const = 0;
+    // virtual void UnbindTexture() const = 0;
 
-		virtual void Resize(uint32_t width, uint32_t height) = 0;
+    virtual const FrameBufferSpecification &GetSpecification() const = 0;
 
-		virtual void ReadPixel(uint32_t attachmentIndex, int x, int y, void* data) = 0;
-		virtual void ReadPixels(uint32_t attachmentIndex, int x, int y, int width, int height, void* data) = 0;
-
-		virtual void ClearColorAttachment(int index, const void* value) = 0;
-
-		virtual uint32_t GetColorAttachmentRendererID(int index = 0) const = 0;
-		virtual void BindColorAttachmentTexture(int index = 0) const = 0;
-		//virtual void UnbindTexture() const = 0;
-
-		virtual const FrameBufferSpecification& GetSpecification() const = 0;
-
-		static Ref<FrameBuffer> Create(const FrameBufferSpecification& spec);
-	};
-}
+    static Ref<FrameBuffer> Create(const FrameBufferSpecification &spec);
+};
+} // namespace Pyxis
